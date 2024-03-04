9a62df3cbd7bb82dc72b4727a55041e3ad29ecb9
Domenico Siciliani <d.siciliani96@gmail.com>
2024-03-04 13:22:09 +0100

    Improved example main

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ if __name__ == "__main__":
     newsgroups_train = fetch_20newsgroups(subset='train', categories=CATS, data_home="./data/20news")
     newsgroups_test = fetch_20newsgroups(subset='test', categories=CATS, data_home="./data/20news")
 
+    sample = newsgroups_test.data[0]
+
     model = GZipKNN(n_neighbors=K)
     model.fit(newsgroups_train.data, newsgroups_train.target)
-    distances, top_k_idx = model.kneighbors(newsgroups_test.data[0])
-    print(distances, top_k_idx)
+    distances, top_k_idx = model.kneighbors(sample)
     
     for i in top_k_idx:
         print(newsgroups_train.data[i])
         print("__________________________")
+    
+    print(distances, top_k_idx)
     print(newsgroups_train.target[top_k_idx])
 
-    preds = model.predict([newsgroups_test.data[0]])
+    preds = model.predict([sample])
     print(preds, [newsgroups_train.target_names[pred] for pred in preds])
