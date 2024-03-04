eec4adecc50dcac06d955594a14a3e29a3fb0117
Domenico Siciliani <d.siciliani96@gmail.com>
2024-03-04 13:03:58 +0100

    Added kneighbors method

--- gzip_knn.py
+++ gzip_knn.py
@@ -9,22 +9,34 @@ class GZipKNN:
     def fit(self, X: Iterable[str], y: Iterable[int]):
         self.X = X
         self.y = y
+
+    def kneighbors(self, x1, n_neighbors=None, return_distance=True):
+        if n_neighbors is None:
+            n_neighbors = self.n_neighbors
+        Cx1 = len(gzip.compress(x1.encode()))
+        distance_from_x1 = []
+
+        for x2 in self.X:
+            Cx2 = len(gzip.compress(x2.encode()))
+            x1x2 = " ".join([x1, x2])
+            Cx1x2 = len(gzip.compress(x1x2.encode()))
+            ncd = (Cx1x2 - min(Cx1, Cx2)) / max(Cx1, Cx2) # Normalized Compression Distance
+            distance_from_x1.append(ncd)
+
+        distance_from_x1 = np.array(distance_from_x1)
+        sorted_idx = np.argsort(distance_from_x1)
+        top_k_idx = sorted_idx[:n_neighbors]
+        print(top_k_idx)
+        if return_distance:
+            return distance_from_x1[top_k_idx], top_k_idx
+        else:
+            return top_k_idx
     
     def predict(self, X: Iterable[str]):
         predictions = []
         for x1 in X:
-            Cx1 = len(gzip.compress(x1.encode()))
-            distance_from_x1 = []
-
-            for x2 in self.X:
-                Cx2 = len(gzip.compress(x2.encode()))
-                x1x2 = " ".join([x1, x2])
-                Cx1x2 = len(gzip.compress(x1x2.encode()))
-                ncd = (Cx1x2 - min(Cx1, Cx2)) / max(Cx1, Cx2) # Normalized Compression Distance
-                distance_from_x1.append(ncd)
-
-            sorted_idx = np.argsort(np.array(distance_from_x1))
-            top_k_classes = self.y[sorted_idx[:self.n_neighbors]].tolist()
+            top_k_idx = self.kneighbors(x1, return_distance=False)
+            top_k_classes = self.y[top_k_idx].tolist()
             predict_class = max(set(top_k_classes), key=top_k_classes.count)
             predictions.append(predict_class)
         return predictions
